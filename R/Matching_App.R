b522793a5a0b3d855ade9aab1d4ecf4aba487f62
ben_bellman <benjamin_bellman@brown.edu>
2018-09-13 16:14:53 -0400

    Start app doc

---
+++ R/Matching_App.R
@@ -0,0 +1,21 @@
+library(shiny)
+library(here)
+
+# Load the data here
+
+# Define UI for application that plots features of movies 
+ui <- fluidPage(
+  
+  # Set the layout here, and everntually add input and output features 
+  
+  
+)
+
+# Define server function required to generate outputs
+server <- function(input, output) {
+  
+
+}
+
+# Create a Shiny app object
+shinyApp(ui = ui, server = server)
